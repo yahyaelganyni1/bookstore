b70282952a67e3d7dc58ef99499c1458d5bd5b1c
yahya <crazy20046@gmail.com>
2021-11-11 01:54:54 +0200

    Fix linters

--- src/components/BookPages.js
+++ src/components/BookPages.js
@@ -2,13 +2,11 @@ import React from 'react';
 import SingleBook from './SingleBook';
 import BookForm from './BookForm';
 
-const BookPages = () => {
-  return (
-    <div>
-      <SingleBook />
-      <BookForm />
-    </div>
-  );
-};
+const BookPages = () => (
+  <div>
+    <SingleBook />
+    <BookForm />
+  </div>
+);
 
 export default BookPages;
--- src/components/SingleBook.js
+++ src/components/SingleBook.js
@@ -1,17 +1,15 @@
 import React from 'react';
 
-const SingleBook = () => {
-  return (
-    <div>
-      <span>Book Categories</span>
-      <h2>book title</h2>
-      <ul>
-        <li>Comments</li>
-        <li>Remove</li>
-        <li>Edit</li>
-      </ul>
-    </div>
-  );
-};
+const SingleBook = () => (
+  <div>
+    <span>Book Categories</span>
+    <h2>book title</h2>
+    <ul>
+      <li>Comments</li>
+      <li>Remove</li>
+      <li>Edit</li>
+    </ul>
+  </div>
+);
 
 export default SingleBook;
